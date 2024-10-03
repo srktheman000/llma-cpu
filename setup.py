da395b93e3f2984ccc4f2c8967e2eb4820bac2cf
srktheman000 <sohan.kinage000@gmail.com>
2024-10-03 08:03:49 +0530

    feat: added code for the RAG approches and flask ui

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI LLAMA CPU Project',
+    version= '0.0.0',
+    author= 'Sohan',
+    author_email= 'sohan.kinage000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
